aac53352b9308c6f55dfa9327049d2489d5417eb
amanipula <amanipula@gmail.com>
2023-04-03 11:37:43 +0530

    war1 commit

---
+++ src/main/java/com/example/war1/War1Application.java
@@ -0,0 +1,13 @@
+package com.example.war1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class War1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(War1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/war1/War1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.war1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class War1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
